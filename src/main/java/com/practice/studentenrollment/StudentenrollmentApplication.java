22442191a1c83284ea7df465f7d279b2f8e51897
makkenasiva <7842789233@Siva>
2023-06-30 18:59:50 -0400

    second commit

---
+++ src/main/java/com/practice/studentenrollment/StudentenrollmentApplication.java
@@ -0,0 +1,13 @@
+package com.practice.studentenrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentenrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentenrollmentApplication.class, args);
+	}
+
+}
