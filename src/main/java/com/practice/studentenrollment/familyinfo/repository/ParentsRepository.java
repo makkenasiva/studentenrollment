de791bf7a33a7d66e74bd5d6a64c4b4f664037bf
AjayKumarNagula <nagulajay@gmail.com>
2023-07-04 15:31:10 -0400

    Family info

---
+++ src/main/java/com/practice/studentenrollment/familyinfo/repository/ParentsRepository.java
@@ -0,0 +1,8 @@
+package com.practice.studentenrollment.familyinfo.repository;
+
+import com.practice.studentenrollment.familyinfo.model.Parents;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParentsRepository extends JpaRepository<Parents,Integer> {
+
+}
