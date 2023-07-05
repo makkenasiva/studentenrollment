653a26b6529264f30e53305cc721b6d5b7cd13c9
Manoj152A <126439694+Manoj152A@users.noreply.github.com>
2023-07-05 13:21:17 -0400

    Manojupdate (#25)
    * master file was spoiled so made changes required
    
    * master file was spoiled so made changes required

--- src/main/java/com/practice/studentenrollment/familyinfo/repository/ParentsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.practice.studentenrollment.familyinfo.repository;
-
-import com.practice.studentenrollment.familyinfo.model.Parents;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParentsRepository extends JpaRepository<Parents,Integer> {
-
-}
