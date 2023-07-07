6aed7a16e7002b95324dc8b7b96b49e52c03a996
nikhilmittapally <136003160+nikhilmittapally@users.noreply.github.com>
2023-07-07 14:05:29 -0400

    states and institutions updated

---
+++ src/main/resources/java/com/practice/studentenrollment/StateAPI/StateService.java
@@ -0,0 +1,28 @@
+package com.practice.studentenrollment.StateAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StateService {
+    private final StateRepository stateRepository;
+
+    @Autowired
+    public StateService(StateRepository stateRepository) {
+        this.stateRepository = stateRepository;
+    }
+
+    public List<State> getAllStates() {
+        return stateRepository.findAll();
+    }
+
+    public State getStateById(Long id) {
+        Optional<State> stateOptional = stateRepository.findById(id);
+
+        return stateOptional.orElse(null);
+    }
+
+}
