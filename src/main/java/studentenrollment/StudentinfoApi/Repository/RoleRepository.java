847d605c0e7367339e7551a28730f7081b2bb1b8
makkenasiva <136834682+makkenasiva@users.noreply.github.com>
2023-08-02 17:11:11 -0400

    Siva changes (#47)
    * STUDENT INFO , USER CREATION
    
    * master file was spoiled so made changes required
    
    * master file was spoiled so made changes required
    
    * needed changes
    
    * Pushing local changes.
    
    * changes commit
    
    * final commit
    
    * final commit with four updated tables
    
    * updated code role_id,role_name,institution_id
    
    * updates
    
    ---------
    
    Co-authored-by: makkenasiva <7842789233@Siva>
    Co-authored-by: Manoj152A <manoj.chowdary0071@gmail.com>
    Co-authored-by: vikassagi07 <vikassagi07@gmail.com>

--- src/main/java/studentenrollment/StudentinfoApi/Repository/RoleRepository.java
+++ src/main/java/studentenrollment/StudentinfoApi/Repository/RoleRepository.java
@@ -3,11 +3,15 @@ package studentenrollment.StudentinfoApi.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import studentenrollment.StudentinfoApi.Model.Role;
+import studentenrollment.StudentinfoApi.Model.UserRole;
 
+import java.util.List;
 
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    List<Role> findAll();
 
 
 
--- src/main/java/studentenrollment/StudentinfoApi/Service/RoleService.java
+++ src/main/java/studentenrollment/StudentinfoApi/Service/RoleService.java
@@ -3,17 +3,33 @@ package studentenrollment.StudentinfoApi.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import studentenrollment.StudentinfoApi.Model.Role;
+import studentenrollment.StudentinfoApi.Model.UserRole;
 import studentenrollment.StudentinfoApi.Repository.RoleRepository;
+import studentenrollment.StudentinfoApi.Repository.UserRoleRepository;
+
+import java.util.List;
+import java.util.Scanner;
 
 @Service
 public class RoleService {
     @Autowired
     private RoleRepository roleRepository;
 
+    private UserRoleRepository userRoleRepository;
+
     public void insertData(Integer roleId, String roleName) {
         Role role = new Role();
         role.setRole_id(roleId);
         role.setRole_name(roleName);
         roleRepository.save(role);
+    }
+
+    @Autowired
+    public RoleService(UserRoleRepository userRoleRepository) {
+        this.userRoleRepository = userRoleRepository;
+    }
+    public List<Role> getAllRoles() {
+
+        return roleRepository.findAll();
     }
 }
